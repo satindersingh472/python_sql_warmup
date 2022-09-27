eea683768628681ad8f1b1b1ec08cac8700cb8f9
satinder <satindersingh472@gmail.com>
2022-09-27 17:00:05 -0600

    finished

---
+++ app.py
@@ -0,0 +1,43 @@
+import dbcreds
+import mariadb
+
+def sales_persons():
+    conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
+    cursor = conn.cursor()
+    cursor.execute('call sales_persons()')
+    results = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    for result in results:
+        print(result[0],': ' , result[1])
+    pick_salesperson = input('Please pick one of the salesman by typing the number of the salesman : ')
+    return pick_salesperson
+
+
+def buy_something(argument1):
+    conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
+    cursor = conn.cursor()
+    cursor.execute('call items()')
+    results = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    for result in results:
+        print(result[0],': ', result[1], result[2])
+    while(True):
+        pick_item = input('Please choose an item to buy from seller you choose:  ')
+        conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
+        cursor = conn.cursor()
+        cursor.execute('call items_sale(?,?)',[argument1, pick_item])
+        cursor.close()
+        conn.close()
+        print('Item number: ',pick_item, 'Purchased successfully')
+        upsell =  input('Continue Shopping? (yes/no)')
+        if upsell in ['yes']:
+            continue
+        else:
+            print('Thanks for shopping')
+            break
+
+person =  sales_persons()
+buy_something(person)
+    
